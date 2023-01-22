24f06324d1a7c47e6f8bbbc0f8b82fbd16ea04cb
Coyls <loic.grecofr@gmail.com>
2023-01-22 12:23:36 +0100

    refactor(utils): create preloadFrames function

--- src/utils/image-manager.ts
+++ src/utils/image-manager.ts
@@ -1,5 +1,40 @@
-export const getImagePath = (frameFilePath: string, index: number) => {
-    return `${frameFilePath}${index.toString()}.png`;
-  };
+/**
+ * Return the path of a frame
+ *
+ * @param frameFilePath Path to frame without index
+ * @example "/assets/frames/cars_"
+ *
+ * @param index Index frame
+ *
+ *
+ * @param frameExtension Extension file
+ * @example "png, jpg, svg, ..."
+ *
+ */
+export const getFramePath = (
+  frameFilePath: string,
+  index: number,
+  frameExtension: string
+): string => {
+  return `${frameFilePath}${index.toString()}.${frameExtension}`;
+};
 
-export const  duplicateFrame = () => {}
+export const preloadFrames = (
+  frameStarting: number,
+  framesTotal: number,
+  frameFilePath: string,
+  frameExtension: string,
+  canvasWidth: number,
+  canvasHeight: number
+): HTMLImageElement[] => {
+  const imageLoads: HTMLImageElement[] = [];
+  for (let i = frameStarting; i < framesTotal + frameStarting; i++) {
+    const img = new Image(canvasWidth, canvasHeight);
+    img.src = getFramePath(frameFilePath, i, frameExtension);
+    imageLoads.push(img);
+  }
+
+  return imageLoads;
+};
+
+// export const  duplicateFrame = () => {}
